ad5fdb858d6c05825e7c46dedfff6022f3d03fdf
Mohamed Hatem <68520226+mohatem99@users.noreply.github.com>
2024-11-20 14:27:00 +0200

    first commit

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import SideNav from "../ui/dashboard/sidenav";
+
+const Layout = ({
+  children,
+}: {
+  children: React.ReactNode; // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children component to render within the layout.  // The children
+}) => {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64 ">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
